7afbc4700164011b215937968d5d64b455ad398a
philip-bst <philip@rvlife.com>
2023-11-29 18:49:33 +0545

    chore: add click test

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -1,8 +1,22 @@
 import { Button } from "./Button";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-it("renders a button with the text 'Click me!'", () => {
-  render(<Button />);
+describe("Button", () => {
+  it("should render", () => {
+    render(<Button />);
 
-  expect(screen.getByText("Click me!")).toBeInTheDocument();
+    expect(screen.getByText("Click me!")).toBeInTheDocument();
+  });
+
+  it("should trigger onClick when clicked", () => {
+    const mockOnClick = jest.fn();
+
+    render(<Button onClick={mockOnClick} />);
+
+    screen.getByRole("button", { name: /click me!/i }).click();
+
+    expect(mockOnClick).toHaveBeenCalled();
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
+  });
 });
