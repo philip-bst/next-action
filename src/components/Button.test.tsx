75e7a842ada087826c6bc1d0fc6e7cc82cfe6f9e
philip-bst <philip@rvlife.com>
2023-11-29 18:38:31 +0545

    make run fail

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -4,5 +4,5 @@ import { render, screen } from "@testing-library/react";
 it("renders a button with the text 'Click me!'", () => {
   render(<Button />);
 
-  expect(screen.getByText("Click me!")).toBeInTheDocument();
+  expect(screen.getByText("Click h me!")).toBeInTheDocument();
 });
