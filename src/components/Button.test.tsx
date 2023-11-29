bf36ad15ec5744b83937cd83dd369e6eb5872a62
philip-bst <philip@rvlife.com>
2023-11-29 18:50:45 +0545

    chore: remove unused

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -1,6 +1,5 @@
 import { Button } from "./Button";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 
 describe("Button", () => {
   it("should render", () => {
