1ce3d18c1f846416df49c6f358bfa14aceaa5ff7
philip-bst <philip@rvlife.com>
2023-11-29 18:36:12 +0545

    chore: add testing to workflow

---
+++ src/components/Button.test.tsx
@@ -0,0 +1,8 @@
+import { Button } from "./Button";
+import { render, screen } from "@testing-library/react";
+
+it("renders a button with the text 'Click me!'", () => {
+  render(<Button />);
+
+  expect(screen.getByText("Click me!")).toBeInTheDocument();
+});
